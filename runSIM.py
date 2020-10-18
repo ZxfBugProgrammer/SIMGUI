6e1e22019151bb3045970e5f54b3aa9624e5c36d
ZXF <1150123839@qq.com>
2020-10-18 19:38:06 +0800

    fix bug

--- runSIM.py
+++ runSIM.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import os
+import subprocess
 
 
 def runSIM(mode, exePath, argv):
@@ -9,5 +10,9 @@ def runSIM(mode, exePath, argv):
     nowPath = os.path.join(nowPath, fileName)
     # print(nowPath)
     argv = nowPath + ' ' + argv
-    res = os.popen(argv)
-    res.close()
+    # res = os.popen(argv)
+    # res.close()
+    p = subprocess.Popen(argv, stdin=subprocess.PIPE,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+    p.wait()
+    return
